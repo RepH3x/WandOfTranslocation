004f7c656b2116cafb6e6a94c22b8b6f75eb4c33
Samual <lewissamuala@gmail.com>
2024-01-16 20:06:11 -0800

    Initial Setup

---
+++ src/client/java/com/reph3x/wot/WandOfTranslocationClient.java
@@ -0,0 +1,10 @@
+package com.reph3x.wot;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WandOfTranslocationClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
