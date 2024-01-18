c7503e7c73c9a88471722cbb83938d12e8e42809
Samual <lewissamuala@gmail.com>
2024-01-17 22:57:49 -0800

    Inventory moving capablilities functional. Can move single chests and barrels and store inventories inside the wand. Contents of the wand are saved and can even be nested inside each other.

--- src/main/java/com/reph3x/wot/item/custom/WandOfTranslocationItem.java
+++ src/main/java/com/reph3x/wot/item/custom/WandOfTranslocationItem.java
@@ -2,12 +2,16 @@ package com.reph3x.wot.item.custom;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BarrelBlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -23,9 +27,12 @@ import java.util.Random;
 
 public class WandOfTranslocationItem extends Item {
 //TODO: Chests don't merge with existing chests when placed currently, and chests and barrels do not face the way one would expect
-//TODO: Does not yet transfer inventories or any data between chests at all
+//TODO: Make name of most plentiful item in stored inventory to show on tooltip
     public static final String FULL_KEY = "isfull";
-    public static final String TYPE_KEY = "inventorytype"; // 0 = chest, 1 = barrel
+    public static final String TYPE_KEY = "wandinventorytype"; // 0 = chest, 1 = barrel
+    public static final String INVENTORY_KEY = "wandinventory";
+    public static final int CHEST_SLOT_COUNT = 27;
+    public static final int DOUBLE_CHEST_SLOT_COUNT = 54;
 
     public WandOfTranslocationItem(Settings settings) {
         super(settings);
@@ -105,105 +112,143 @@ public class WandOfTranslocationItem extends Item {
     private boolean wandContainsChest(NbtCompound stackNbt) { return stackNbt.contains(TYPE_KEY) && stackNbt.getInt(TYPE_KEY) == 0; }
     private boolean wandContainsBarrel(NbtCompound stackNbt) { return stackNbt.contains(TYPE_KEY) && stackNbt.getInt(TYPE_KEY) == 1; }
 
-    private boolean handleChestPlacement(World world, BlockPos positionClicked, PlayerEntity player, NbtCompound stackNbt) {
+    private boolean handleChestPlacement(World world, BlockPos pos, PlayerEntity player, NbtCompound stackNbt) {
         if(!world.isClient()) {
-            if(determineChestPlacement(world, positionClicked, player, stackNbt)) {
+            if(determineChestPlacement(world, pos, player, stackNbt)) {
                 stackNbt.putBoolean(FULL_KEY, false);
-                world.setBlockState(positionClicked.up(), Blocks.CHEST.getDefaultState());
+                world.setBlockState(pos.up(), Blocks.CHEST.getDefaultState());
+                populateChest(world, pos.up(), stackNbt);
                 return true;
             }
         } else {
-            if(determineChestPlacement(world, positionClicked, player, stackNbt)) {
-                spawnParticles(world, positionClicked);
-                world.playSound(player, positionClicked, SoundEvents.BLOCK_WOOD_PLACE, SoundCategory.BLOCKS, 1, 0);
+            if(determineChestPlacement(world, pos, player, stackNbt)) {
+                spawnParticles(world, pos);
+                world.playSound(player, pos, SoundEvents.BLOCK_WOOD_PLACE, SoundCategory.BLOCKS, 1, 0);
                 return true;
             }
         }
         return false;
     }
-    private boolean handleBarrelPlacement(World world, BlockPos positionClicked, PlayerEntity player, NbtCompound stackNbt) {
+    private boolean handleBarrelPlacement(World world, BlockPos pos, PlayerEntity player, NbtCompound stackNbt) {
         if(!world.isClient()) {
-            if(determineBarrelPlacement(world, positionClicked, player, stackNbt)) {
+            if(determineBarrelPlacement(world, pos, player, stackNbt)) {
                 stackNbt.putBoolean(FULL_KEY, false);
-                world.setBlockState(positionClicked.up(), Blocks.BARREL.getDefaultState());
+                world.setBlockState(pos.up(), Blocks.BARREL.getDefaultState());
+                populateBarrel(world, pos.up(), stackNbt);
                 return true;
             }
         } else {
-            if(determineBarrelPlacement(world, positionClicked, player, stackNbt)) {
-                spawnParticles(world, positionClicked);
-                world.playSound(player, positionClicked, SoundEvents.BLOCK_WOOD_PLACE, SoundCategory.BLOCKS, 1,0);
+            if(determineBarrelPlacement(world, pos, player, stackNbt)) {
+                spawnParticles(world, pos);
+                world.playSound(player, pos, SoundEvents.BLOCK_WOOD_PLACE, SoundCategory.BLOCKS, 1,0);
                 return true;
             }
         }
         return false;
     }
-
-    private void handleChestRemoval(World world, BlockPos positionClicked, PlayerEntity player, NbtCompound stackNbt) {
-        if(!world.isClient()) fillWandWithChest(world, positionClicked, stackNbt);
+    private void handleChestRemoval(World world, BlockPos pos, PlayerEntity player, NbtCompound stackNbt) {
+        if(!world.isClient()) {
+            populateWandWithChest(world, pos, stackNbt);
+            world.removeBlock(pos, false);
+        }
         if(world.isClient()) {
-            spawnParticles(world, positionClicked);
-            world.playSound(player, positionClicked, SoundEvents.BLOCK_WOOD_BREAK, SoundCategory.BLOCKS, 1, 0);
+            spawnParticles(world, pos);
+            world.playSound(player, pos, SoundEvents.BLOCK_WOOD_BREAK, SoundCategory.BLOCKS, 1, 0);
+        }
+
+    }
+    private void handleBarrelRemoval(World world, BlockPos pos, PlayerEntity player, NbtCompound stackNbt) {
+        if(!world.isClient()) {
+            populateWandWithBarrel(world, pos, stackNbt);
+            world.removeBlock(pos, false);
+        }
+        if(world.isClient()) {
+            spawnParticles(world, pos);
+            world.playSound(player, pos, SoundEvents.BLOCK_WOOD_BREAK, SoundCategory.BLOCKS, 1, 0);
         }
     }
-    private void handleBarrelRemoval(World world, BlockPos positionClicked, PlayerEntity player, NbtCompound stackNbt) {
-        if(!world.isClient()) fillWandWithBarrel(world, positionClicked, stackNbt);
-        if(world.isClient()) {
-            spawnParticles(world, positionClicked);
-            world.playSound(player, positionClicked, SoundEvents.BLOCK_WOOD_BREAK, SoundCategory.BLOCKS, 1, 0);
-        }
-    }
+    private boolean determineChestPlacement(World world, BlockPos pos, PlayerEntity player, NbtCompound stackNbt) {
+        BlockState blockAbove = world.getBlockState(pos.up());
+        BlockState blockTwoAbove = world.getBlockState(pos.up(2));
 
-    private boolean determineChestPlacement(World world, BlockPos positionClicked, PlayerEntity player, NbtCompound stackNbt) {
-        BlockState blockAboveClicked = world.getBlockState(positionClicked.up());
-        BlockState blockTwoAboveClicked = world.getBlockState(positionClicked.up(2));
-
-        if(!blockAboveClicked.isAir()) {
+        if(!blockAbove.isAir()) {
             if(!world.isClient()) player.sendMessage(Text.literal("Invalid Chest Placement: Block For Chest Is Not Air.").formatted(Formatting.RED), false);
             return false;
         }
-        if(blockTwoAboveClicked.isSolidBlock(world, positionClicked.up(2))) {
+        if(blockTwoAbove.isSolidBlock(world, pos.up(2))) {
             if(!world.isClient()) player.sendMessage(Text.literal("Invalid Chest Placement: Block Above Chest Is Solid.").formatted(Formatting.RED), false);
             return false;
         }
         //I don't like how dirty these messages are but too small brain to figure out how else to do this only on server while still giving good feedback short of using another NBT tag
         return true;
     }
-    private boolean determineBarrelPlacement(World world, BlockPos positionClicked, PlayerEntity player, NbtCompound stackNbt) {
-        BlockState blockAboveClicked = world.getBlockState(positionClicked.up());
-        if(!blockAboveClicked.isAir()) {
+    private boolean determineBarrelPlacement(World world, BlockPos pos, PlayerEntity player, NbtCompound stackNbt) {
+        BlockState blockAbove = world.getBlockState(pos.up());
+        if(!blockAbove.isAir()) {
             if(!world.isClient()) player.sendMessage(Text.literal("Invalid Barrel Placement: Block For Barrel Is Not Air.").formatted(Formatting.RED), false);
             return false;
         }
 
         stackNbt.putBoolean(FULL_KEY, false);
-        world.setBlockState(positionClicked.up(), Blocks.BARREL.getDefaultState());
+        world.setBlockState(pos.up(), Blocks.BARREL.getDefaultState());
         return true;
     }
 
-    private void fillWandWithChest(World world, BlockPos positionClicked, NbtCompound stackNbt) {
+    private void populateWandWithChest(World world, BlockPos pos, NbtCompound stackNbt) {
+        ChestBlockEntity chest = (ChestBlockEntity)world.getBlockEntity(pos);
+        SimpleInventory inventory = new SimpleInventory(CHEST_SLOT_COUNT);
+        for(int i = 0; i < chest.size(); i++) {
+            if(!world.isClient()) {
+                inventory.setStack(i, chest.getStack(i).copy());
+            }
+        }
         stackNbt.putBoolean(FULL_KEY, true);
         stackNbt.putInt(TYPE_KEY, 0);
-        world.removeBlock(positionClicked, false);
+        stackNbt.put(INVENTORY_KEY, inventory.toNbtList());
+        chest.clear();
     }
-    private void fillWandWithBarrel(World world, BlockPos positionClicked, NbtCompound stackNbt) {
+    private void populateWandWithBarrel(World world, BlockPos pos, NbtCompound stackNbt) {
+        BarrelBlockEntity barrel = (BarrelBlockEntity)world.getBlockEntity(pos);
+        SimpleInventory inventory = new SimpleInventory(CHEST_SLOT_COUNT);
+        for(int i = 0; i < barrel.size(); i++) {
+            if(!world.isClient()) {
+                inventory.setStack(i, barrel.getStack(i).copy());
+            }
+        }
         stackNbt.putBoolean(FULL_KEY, true);
         stackNbt.putInt(TYPE_KEY, 1);
-        world.removeBlock(positionClicked, false);
+        stackNbt.put(INVENTORY_KEY, inventory.toNbtList());
+        barrel.clear();
     }
 
-    private void spawnParticles(World world, BlockPos positionClicked) {
+    private void populateChest(World world, BlockPos pos, NbtCompound stackNbt) {
+        SimpleInventory inventory = new SimpleInventory(CHEST_SLOT_COUNT);
+        inventory.readNbtList((NbtList)stackNbt.get(INVENTORY_KEY));
+        ChestBlockEntity chest = (ChestBlockEntity)world.getBlockEntity(pos);
+        for(int i = 0; i < chest.size(); i++) {
+            chest.setStack(i, inventory.getStack(i).copy());
+        }
+    }
+    private void populateBarrel(World world, BlockPos pos, NbtCompound stackNbt) {
+        SimpleInventory inventory = new SimpleInventory(CHEST_SLOT_COUNT);
+        inventory.readNbtList((NbtList)stackNbt.get(INVENTORY_KEY));
+        BarrelBlockEntity barrel = (BarrelBlockEntity)world.getBlockEntity(pos);
+        for(int i = 0; i < barrel.size(); i++) {
+            barrel.setStack(i, inventory.getStack(i).copy());
+        }
+    }
+
+    private void spawnParticles(World world, BlockPos pos) {
         Random rand = new Random();
         for(int i = 0; i <= rand.nextInt(3,7); i++) {
-            double x = positionClicked.getX() + 0.5 + rand.nextDouble(0.5);
-            double y = positionClicked.getY() + 0.5 + rand.nextDouble(0.5);
-            double z = positionClicked.getZ() + 0.5 + rand.nextDouble(0.5);
-            double xA = rand.nextDouble(-0.2, 0.2);
-            double yA = rand.nextDouble(-0.2, 0.2);
-            double zA = rand.nextDouble(-0.2, 0.2);
+            double x = pos.getX() + 0.5 + rand.nextDouble(0.5);
+            double y = pos.getY() + 0.5 + rand.nextDouble(0.5);
+            double z = pos.getZ() + 0.5 + rand.nextDouble(0.5);
+            double xA = rand.nextDouble(0, 0.2);
+            double yA = rand.nextDouble(0, 0.2);
+            double zA = rand.nextDouble(0, 0.2);
             world.addParticle(ParticleTypes.POOF, x, y, z, xA, yA, zA);
         }
     }
 
 }
-
-
